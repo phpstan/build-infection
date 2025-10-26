b1fd8cf16871ec348d816a59c3f00bfcb29b7fa7
Markus Staab <maggus.staab@googlemail.com>
2025-10-26 10:21:46 +0100

    Don't double negate

--- src/Infection/TrinaryLogicMutator.php
+++ src/Infection/TrinaryLogicMutator.php
@@ -5,12 +5,13 @@ namespace PHPStan\Infection;
 use Infection\Mutator\Definition;
 use Infection\Mutator\Mutator;
 use Infection\Mutator\MutatorCategory;
+use Infection\PhpParser\Visitor\ParentConnector;
 use LogicException;
 use PhpParser\Node;
 use function in_array;
 
 /**
- * @implements Mutator<Node\Expr\MethodCall>
+ * @implements Mutator<Node\Expr\MethodCall|Node\Expr\BooleanNot>
  */
 final class TrinaryLogicMutator implements Mutator
 {
@@ -38,6 +39,18 @@ final class TrinaryLogicMutator implements Mutator
 
 	public function canMutate(Node $node): bool
 	{
+		if ($node instanceof Node\Expr\MethodCall) {
+			$parentNode = ParentConnector::getParent($node);
+
+			if ($parentNode instanceof Node\Expr\BooleanNot) {
+				return false;
+			}
+		}
+
+		if ($node instanceof Node\Expr\BooleanNot) {
+			$node = $node->expr;
+		}
+
 		if (!$node instanceof Node\Expr\MethodCall) {
 			return false;
 		}
@@ -55,6 +68,25 @@ final class TrinaryLogicMutator implements Mutator
 
 	public function mutate(Node $node): iterable
 	{
+		if ($node instanceof Node\Expr\BooleanNot) {
+			$node = $node->expr;
+			if (!$node instanceof Node\Expr\MethodCall) {
+				throw new LogicException();
+			}
+
+			if (!$node->name instanceof Node\Identifier) {
+				throw new LogicException();
+			}
+
+			if ($node->name->name === 'yes') {
+				yield new Node\Expr\MethodCall($node->var, 'no');
+			} else {
+				yield new Node\Expr\MethodCall($node->var, 'yes');
+			}
+
+			return;
+		}
+
 		if (!$node->name instanceof Node\Identifier) {
 			throw new LogicException();
 		}
