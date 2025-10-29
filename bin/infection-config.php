437beddcded98c1522a185328400c43b2d579302
Markus Staab <maggus.staab@googlemail.com>
2025-10-29 11:10:25 +0100

    cover with phpstan

--- bin/infection-config.php
+++ bin/infection-config.php
@@ -1,19 +1,21 @@
 #!/usr/bin/env php
 <?php declare(strict_types = 1);
 
+// Usage: php infection-config.php [--source-directory='another/path'] [--mutator-class='Infection\Mutator\Removal\MethodCallRemoval'] [--timeout=60]
+
 error_reporting(E_ALL & ~E_DEPRECATED);
 ini_set('display_errors', 'stderr');
 
 $opts = getopt('', ['source-directory::', 'mutator-class::', 'timeout::']);
-if ($argc < 1) {
-	echo "Usage: php ". $argv[0] ." [--source-directory='another/path'] [--mutator-class='Infection\Mutator\Removal\MethodCallRemoval] [--timeout=60]'\n";
-	exit(1);
-}
 $addSourceDirectories = (array) ($opts['source-directory'] ?? []);
 $addMutatorClasses = (array) ($opts['mutator-class'] ?? []);
 $timeout = $opts['timeout'] ?? null;
 
-$decoded = json_decode(file_get_contents(__DIR__.'/../resources/infection.json5'));
+$defaults = file_get_contents(__DIR__.'/../resources/infection.json5');
+if ($defaults === false) {
+	throw new RuntimeException('Unable to read infection.json5');
+}
+$decoded = json_decode($defaults);
 foreach($addSourceDirectories as $path) {
 	$decoded->source->directories[] = $path;
 }
