96458d8e015c837aa09fe024264457c1cb248324
Markus Staab <markus.staab@redaxo.de>
2025-11-11 14:51:55 +0100

    Implement TrueTruthyFalseFalseyTypeSpecifierContextMutator

---
+++ tests/Infection/TrueTruthyFalseFalseyTypeSpecifierContextMutatorTest.php
@@ -0,0 +1,106 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Infection;
+
+use Infection\Testing\BaseMutatorTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+#[CoversClass(TrueTruthyFalseFalseyTypeSpecifierContextMutator::class)]
+final class TrueTruthyFalseFalseyTypeSpecifierContextMutatorTest extends BaseMutatorTestCase
+{
+
+	/**
+	 * @param string|string[] $expected
+	 */
+	#[DataProvider('mutationsProvider')]
+	public function testMutator(string $input, $expected = []): void
+	{
+		$this->assertMutatesInput($input, $expected);
+	}
+
+	/**
+	 * @return iterable<string, array{0: string, 1?: string}>
+	 */
+	public static function mutationsProvider(): iterable
+	{
+		yield 'It mutates true()' => [
+			<<<'PHP'
+				<?php
+				$context = TypeSpecifierContext::createNull();
+				$context->true();
+				PHP
+,
+			<<<'PHP'
+				<?php
+
+				$context = TypeSpecifierContext::createNull();
+				$context->truthy();
+				PHP
+,
+		];
+
+		yield 'It mutates truthy()' => [
+			<<<'PHP'
+				<?php
+				$context = TypeSpecifierContext::createNull();
+				$context->truthy();
+				PHP
+,
+			<<<'PHP'
+				<?php
+
+				$context = TypeSpecifierContext::createNull();
+				$context->true();
+				PHP
+,
+		];
+
+		yield 'It mutates false()' => [
+			<<<'PHP'
+				<?php
+				$context = TypeSpecifierContext::createNull();
+				$context->false();
+				PHP
+,
+			<<<'PHP'
+				<?php
+
+				$context = TypeSpecifierContext::createNull();
+				$context->falsey();
+				PHP
+,
+		];
+
+		yield 'It mutates falsey()' => [
+			<<<'PHP'
+				<?php
+				$context = TypeSpecifierContext::createNull();
+				$context->falsey();
+				PHP
+,
+			<<<'PHP'
+				<?php
+
+				$context = TypeSpecifierContext::createNull();
+				$context->false();
+				PHP
+,
+		];
+
+		yield 'It skips null()' => [
+			<<<'PHP'
+				<?php
+				$context = TypeSpecifierContext::createNull();
+				$context->null();
+				PHP
+,
+		];
+	}
+
+	protected function getTestedMutatorClassName(): string
+	{
+		return TrueTruthyFalseFalseyTypeSpecifierContextMutator::class;
+	}
+
+}
